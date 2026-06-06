d1fabf81606fd382313cc17ad996f929db1668df
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-06 21:34:50 +0330

    create : deduplication processor

---
+++ internal/pipeline/deduplication.go
@@ -0,0 +1,53 @@
+package pipeline
+
+import (
+	"DataCollector/internal/models"
+	"DataCollector/internal/storage"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
+type DeduplicationProcessor struct {
+	store storage.HashStore
+}
+
+func NewDeduplicationProcessor(
+	store storage.HashStore,
+) *DeduplicationProcessor {
+
+	return &DeduplicationProcessor{
+		store: store,
+	}
+}
+
+func getText(
+	doc models.Document,
+) string {
+	return fmt.Sprint(
+		doc.Content,
+	)
+}
+
+func (p *DeduplicationProcessor) Process(
+	doc models.Document,
+) (models.Document, bool) {
+
+	text := getText(doc)
+
+	sum := sha256.Sum256(
+		[]byte(text),
+	)
+
+	hash := hex.EncodeToString(
+		sum[:],
+	)
+
+	if p.store.Exists(hash) {
+		return doc, false
+	}
+
+	_ = p.store.Add(hash)
+
+	return doc, true
+}
